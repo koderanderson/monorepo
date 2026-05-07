d3ca7dbdb33cb97ad5339cd242c3a5af237ec34c
Anderson Soares <anderson@emutuadigital.com>
2026-05-07 03:49:08 -0300

    dokploy

--- dokploy/docker-entrypoint-api.sh
+++ dokploy/docker-entrypoint-api.sh
@@ -6,4 +6,7 @@ if [ -f composer.json ] && [ ! -f vendor/autoload.php ]; then
   composer install --ignore-platform-reqs --no-dev --no-interaction --prefer-dist --no-progress --optimize-autoloader --no-scripts
 fi
 
+chmod -R 775 /app/storage /app/bootstrap/cache
+chown -R www-data:www-data /app/storage /app/bootstrap/cache
+
 exec docker-php-entrypoint "$@"
